39bff7aa15c470866a92a6b4f28399261dd6e060
smyoo <proysm5154@naver.com>
2024-10-28 23:45:26 +0900

    Initial commit

---
+++ src/test/java/dgu/cse/newsee/NewseeApplicationTests.java
@@ -0,0 +1,13 @@
+package dgu.cse.newsee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewseeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
