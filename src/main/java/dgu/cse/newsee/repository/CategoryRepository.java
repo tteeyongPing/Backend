3d62aa2e85cdf056d58971624ccc781ea5f430a7
안소희 <2022112701@dgu.ac.kr>
2024-11-12 14:10:40 +0900

    feat #7 관심분야 설정

--- src/main/java/dgu/cse/newsee/repository/CategoryRepository.java
+++ src/main/java/dgu/cse/newsee/repository/CategoryRepository.java
@@ -1,10 +1,10 @@
 package dgu.cse.newsee.repository;
 
 
-import dgu.cse.newsee.domain.entity.CategoryEntity;
+import dgu.cse.newsee.domain.enums.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CategoryRepository extends JpaRepository<CategoryEntity, Long> {
+public interface CategoryRepository extends JpaRepository<Category, Long> {
 }
