57b0d21ad1ccc0472a2579edad8041907fbd86ab
안소희 <2022112701@dgu.ac.kr>
2024-11-20 18:26:02 +0900

    feat #25 에러 처리

--- src/main/java/dgu/cse/newsee/app/controller/CategoryController.java
+++ src/main/java/dgu/cse/newsee/app/controller/CategoryController.java
@@ -3,6 +3,7 @@ package dgu.cse.newsee.app.controller;
 
 import dgu.cse.newsee.apiPayload.Status;
 import dgu.cse.newsee.apiPayload.ApiResponse;
+import dgu.cse.newsee.apiPayload.exception.UserException;
 import dgu.cse.newsee.service.category.CategoryService;
 import dgu.cse.newsee.domain.enums.Category;
 import dgu.cse.newsee.app.dto.CategoryDto;
@@ -39,17 +40,22 @@ public class CategoryController {
     // 내 관심분야 가져오기
     @GetMapping("/my")
     public ApiResponse<Object> getMyCategories(@RequestParam Long userId) {
-        List<Category> categories = categoryService.getUserCategories(userId);
-        List<CategoryDto> data = categories.stream()
-                .map(CategoryDto::new)
-                .collect(Collectors.toList());
+        try {
+            List<Category> categories = categoryService.getUserCategories(userId);
+            List<CategoryDto> data = categories.stream()
+                    .map(CategoryDto::new)
+                    .collect(Collectors.toList());
 
-        return ApiResponse.onSuccess(Status.CATEGORY_MY_SUCCESS, data);
+            return ApiResponse.onSuccess(Status.CATEGORY_MY_SUCCESS, data);
+        }catch (UserException.UserNonExistsException ex){
+            throw new UserException.UserNonExistsException("등록되지 않은 사용자입니다.");
+        }
     }
 
     // 내 관심분야 등록/수정하기
     @PatchMapping("/edit")
     public ApiResponse<Object> editMyCategories(@RequestParam Long userId, @RequestBody List<String> categoryIds) {
+        try{
         // categoryIds를 Category 객체로 변환
         List<Category> categories = categoryIds.stream()
                 .map(Category::fromStringId) // fromId 메서드가 String을 받아 Category 반환
@@ -58,6 +64,10 @@ public class CategoryController {
         // Service 호출
         categoryService.updateUserCategories(userId, categories);
         return ApiResponse.onSuccess(Status.CATEGORY_EDIT_SUCCESS, null);
+        }catch (UserException.UserNonExistsException ex){
+            throw new UserException.UserNonExistsException("등록되지 않은 사용자입니다.");
+        }
+
     }
 
 
--- src/main/java/dgu/cse/newsee/service/category/CategoryService.java
+++ src/main/java/dgu/cse/newsee/service/category/CategoryService.java
@@ -1,7 +1,9 @@
 package dgu.cse.newsee.service.category;
 
+import dgu.cse.newsee.apiPayload.exception.UserException;
 import dgu.cse.newsee.domain.enums.Category;
 import dgu.cse.newsee.repository.UserCategoryRepository;
+import dgu.cse.newsee.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,10 +14,12 @@ import java.util.List;
 public class CategoryService {
 
     private final UserCategoryRepository userCategoryRepository;
+    private final UserRepository userRepository;
 
     @Autowired
-    public CategoryService(UserCategoryRepository userCategoryRepository) {
+    public CategoryService(UserCategoryRepository userCategoryRepository, UserRepository userRepository) {
         this.userCategoryRepository = userCategoryRepository;
+        this.userRepository = userRepository;
     }
 
     // 모든 관심분야 가져오기
@@ -25,11 +29,17 @@ public class CategoryService {
 
     // 사용자의 관심분야 가져오기
     public List<Category> getUserCategories(Long userId) {
+        if (!userRepository.existsById(userId)) {
+            throw new UserException.UserNonExistsException("등록되지 않은 사용자입니다.");
+        }
         return userCategoryRepository.findCategoriesByUserId(userId);
     }
 
     // 사용자의 관심분야 수정하기
     public void updateUserCategories(Long userId, List<Category> categories) {
+        if (!userRepository.existsById(userId)) {
+            throw new UserException.UserNonExistsException("등록되지 않은 사용자입니다.");
+        }
         // 기존 관심 분야 삭제
         userCategoryRepository.deleteByUserId(userId);
 
