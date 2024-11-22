e7e0b62c5e47902f6bea5afc7211b26d829437f6
smyoo <proysm5154@naver.com>
2024-11-22 15:41:35 +0900

    prod : 테스트 API 정의

--- src/main/java/dgu/cse/newsee/app/controller/TestController.java
+++ src/main/java/dgu/cse/newsee/app/controller/TestController.java
@@ -2,15 +2,21 @@ package dgu.cse.newsee.app.controller;
 
 import dgu.cse.newsee.apiPayload.ApiResponse;
 import dgu.cse.newsee.apiPayload.Status;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "🧪 테스트", description = "테스트용 API")
 @RequestMapping("api/test")
 public class TestController {
 
+    @Operation(summary = "[사용안함]")
+    @GetMapping("/testing")
     public ApiResponse<?> test(){
         return ApiResponse.onSuccess(Status.TEMP_SUCCESS, "API 테스트 성공");
     }
